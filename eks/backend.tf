11a73f3e9e1b3cdd5ef4b7e792840e510ad08f4d
awsdevopskiran <111186825+awsdevopskiran@users.noreply.github.com>
2024-08-27 15:54:46 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "three-tier-application-backet1"
+    bucket         = "three-tier-application-bucket1"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     # dynamodb_table = "Lock-Files"
