9e80172a004935c70cfd45e2a0d4eefec8edcc0d
awsdevopskiran <111186825+awsdevopskiran@users.noreply.github.com>
2024-08-27 15:51:54 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,19 +1,19 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "three-tier-application-backet1"
-#     region         = "us-east-1"
-#     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-#     dynamodb_table = "Lock-Files"
-#     encrypt        = true
-#   }
-#   required_version = ">=0.13.0"
-#   required_providers {
-#     aws = {
-#       version = ">= 2.7.0"
-#       source  = "hashicorp/aws"
-#     }
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "three-tier-application-backet1"
+    region         = "us-east-1"
+    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    # dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
 provider "aws" {
   region  = var.aws-region
 }
